96fdea3ed0d2768fcd4b0be768c1a2c52fd900f6
pallascodes <pallascodes@gmail.com>
2025-01-10 11:59:05 -0600

    update restaurant api implemented

--- src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -10,49 +10,76 @@ import MenuSection from './MenuSection'
 import ImageSection from './ImageSection'
 import { Button } from '@/components/ui/button'
 import LoadingButton from '@/components/LoadingButton'
+import { Restaurant } from '@/types'
+import { useEffect } from 'react'
 
-const formSchema = z.object({
-  restaurantName: z.string({
-    required_error: 'Restaurant name is required',
-  }),
-  city: z.string({
-    required_error: 'City is required',
-  }),
-  country: z.string({
-    required_error: 'Country is required',
-  }),
-  deliveryPrice: z.coerce.number({
-    required_error: 'Delivery price is required',
-    invalid_type_error: 'Field must be a valid number',
-  }),
-  estimatedDeliveryTime: z.coerce.number({
-    required_error: 'Estimated delivery time is required',
-    invalid_type_error: 'Field must be a valid number',
-  }),
-  cuisines: z.array(z.string()).nonempty({
-    message: 'Please select at lest one item',
-  }),
-  menuItems: z.array(
-    z.object({
-      name: z.string().min(1, 'Name is required'),
-      price: z.coerce.number().min(1, 'Price is required'),
+const formSchema = z
+  .object({
+    restaurantName: z.string({
+      required_error: 'Restaurant name is required',
     }),
-  ),
-  imageFile: z.instanceof(File, { message: 'Image is required' }),
-})
+    city: z.string({
+      required_error: 'City is required',
+    }),
+    country: z.string({
+      required_error: 'Country is required',
+    }),
+    deliveryPrice: z.coerce.number({
+      required_error: 'Delivery price is required',
+      invalid_type_error: 'Field must be a valid number',
+    }),
+    estimatedDeliveryTime: z.coerce.number({
+      required_error: 'Estimated delivery time is required',
+      invalid_type_error: 'Field must be a valid number',
+    }),
+    cuisines: z.array(z.string()).nonempty({
+      message: 'Please select at lest one item',
+    }),
+    menuItems: z.array(
+      z.object({
+        name: z.string().min(1, 'Name is required'),
+        price: z.coerce.number().min(1, 'Price is required'),
+      }),
+    ),
+    imageUrl: z.string().optional(),
+    imageFile: z.instanceof(File, { message: 'Image is required' }).optional(),
+  })
+  .refine((data) => data.imageUrl || data.imageFile, {
+    message: 'Eiher image URL or image File must be provided',
+    path: ['imageFile'],
+  })
 
 type RestaurantFormData = z.infer<typeof formSchema>
 
 type Props = {
   onSave: (restaurantFormData: FormData) => void
   isLoading: boolean
+  restaurant?: Restaurant
 }
 
-export default function ManageRestaurantForm({ onSave, isLoading }: Props) {
+export default function ManageRestaurantForm({ onSave, isLoading, restaurant }: Props) {
   const form = useForm<RestaurantFormData>({
     resolver: zodResolver(formSchema),
     defaultValues: { cuisines: [], menuItems: [{ name: '', price: 1 }] },
   })
+
+  useEffect(() => {
+    if (!restaurant) return
+
+    const deliveryPriceFormatted = parseInt((restaurant.deliveryPrice / 100).toFixed(2))
+    const menuItemsFormatted = restaurant.menuItems.map((item) => ({
+      ...item,
+      price: parseInt((item.price / 100).toFixed(2)),
+    }))
+
+    const updatedRestaurant = {
+      ...restaurant,
+      deliveryPrice: deliveryPriceFormatted,
+      menuItems: menuItemsFormatted,
+    }
+
+    form.reset(updatedRestaurant)
+  }, [form, restaurant])
 
   const onSubmit = (formDataJson: RestaurantFormData) => {
     console.log('first')
@@ -73,7 +100,10 @@ export default function ManageRestaurantForm({ onSave, isLoading }: Props) {
       formData.append(`menuItems[${index}][name]`, menuItem.name)
       formData.append(`menuItems[${index}][price]`, (menuItem.price * 100).toString())
     })
-    formData.append('imageFile', formDataJson.imageFile)
+
+    if (formDataJson.imageFile) {
+      formData.append('imageFile', formDataJson.imageFile)
+    }
 
     onSave(formData)
   }
--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,8 +1,22 @@
-import { useCreateMyRestaurant } from '@/api/MyRestaurantApi'
+import {
+  useCreateMyRestaurant,
+  useGetMyRestaurant,
+  useUpdateMyRestaurant,
+} from '@/api/MyRestaurantApi'
 import ManageRestaurantForm from '@/forms/manage-restaurant-form/ManageRestaurantForm'
 
 export default function ManageRestaurantPage() {
-  const { createRestaurant, isLoading } = useCreateMyRestaurant()
+  const { createRestaurant, isLoading: isCreateLoading } = useCreateMyRestaurant()
+  const { restaurant } = useGetMyRestaurant()
+  const { updateRestaurant, isLoading: isUpdateLoading } = useUpdateMyRestaurant()
 
-  return <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading} />
+  const isEditing = !!restaurant
+
+  return (
+    <ManageRestaurantForm
+      restaurant={restaurant}
+      onSave={isEditing ? updateRestaurant : createRestaurant}
+      isLoading={isCreateLoading || isUpdateLoading}
+    />
+  )
 }
