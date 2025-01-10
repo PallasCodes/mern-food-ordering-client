f33d5dd4415b14a61de2669f14a6558f1adac889
pallascodes <pallascodes@gmail.com>
2025-01-10 17:41:18 -0600

    search api implemented

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,7 +1,30 @@
 import { useParams } from 'react-router-dom'
 
+import { useSearchRestaurants } from '@/api/RestaurantApi'
+import SearchResultInfo from '@/components/SearchResultInfo'
+import SearchResultCard from '@/components/SearchResultCard'
+
 export default function SearchPage() {
   const { city } = useParams()
+  const { results, isLoading } = useSearchRestaurants(city)
 
-  return <div>user searched for {city}</div>
+  if (!results?.data || !city) {
+    return <span>No results found</span>
+  }
+
+  if (isLoading) {
+    return <span>Loading...</span>
+  }
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div id="cuisines-list">insert cuisines here</div>
+      <div id="main-content" className="flex flex-col gap-5">
+        <SearchResultInfo total={results.pagination.total} city={city} />
+        {results.data.map((restaurant) => (
+          <SearchResultCard restaurant={restaurant} />
+        ))}
+      </div>
+    </div>
+  )
 }
